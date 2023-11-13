4e68e9ab9731ecd8ccf027673810385650d1edbf
Michał <147035736+kotekloltrzy@users.noreply.github.com>
2023-11-13 18:23:21 +0100

    Update WDP_10.11.23_4.py

--- Semestr1/WstepDoProgramowania/WDP_10.11.23_4.py
+++ Semestr1/WstepDoProgramowania/WDP_10.11.23_4.py
@@ -1,14 +1,5 @@
-def czy_zawiera(lista1, lista2):  # czy lista 1 zawiera w sobie liste 2
-    print(f'Lista1: {lista1}, Lista2: {lista2}')
-
-    a = 0
-    if all(wyrazy in lista1 for wyrazy in lista2):
-        a = 1
-
-    if a == 1:
-        print("Lista1 zawiera w sobie Listę2.")
-    else:
-        print("Lista1 nie zawiera w sobie Listy2.")
-
-
-czy_zawiera([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [5, 7, 8, 9])
+def czy_zawiera(lista1,lista2,element):
+    for e in lista2:
+        if not czy_należy(lista1,e):
+            return False
+    return True

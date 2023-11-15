4fd0c9b20a4867e7c94b6d34e5f5ab2f9331cb3d
Michał Broda <147035736+kotekloltrzy@users.noreply.github.com>
2023-11-15 11:02:05 +0100

    Update WDP_10.11.23_4.py

--- Semestr1/WstepDoProgramowania/WDP_10.11.23_4.py
+++ Semestr1/WstepDoProgramowania/WDP_10.11.23_4.py
@@ -1,5 +1,18 @@
-def czy_zawiera(lista1,lista2,element):
-    for e in lista2:
-        if not czy_zawiera(lista1,e):
-            return False
-    return True
+lista1 = [5, 3, 1, 4, 2]
+lista2 = [1, 3]
+
+def czy_zawiera(lis1, lis2):
+    ile_zawiera = 0
+    for x2 in lis2:
+        for x1 in lis1:
+            if x2 == x1:
+                ile_zawiera += 1
+                break
+    if ile_zawiera == len(lis2):
+        return True
+    else:
+        return False
+
+print(lista1)
+print(lista2)
+print(czy_zawiera(lista1, lista2))

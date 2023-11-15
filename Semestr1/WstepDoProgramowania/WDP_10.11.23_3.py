af4e7d4113250e738b8f864938d2e52fb9e06a5a
Michał Broda <147035736+kotekloltrzy@users.noreply.github.com>
2023-11-15 08:44:47 +0100

    Update WDP_10.11.23_3.py

--- Semestr1/WstepDoProgramowania/WDP_10.11.23_3.py
+++ Semestr1/WstepDoProgramowania/WDP_10.11.23_3.py
@@ -1,12 +1,8 @@
 def czy_nalezy(element, lista):
-    e = 0
     for e in lista:
         if e == element:
             return True
-        elif e != element:
-            e = e + 1
-    if e > len(lista):
-        return False
+    return False
 
 
-print(czy_nalezy(3, [1, 3, 7]))
+print(czy_nalezy(2, [1, 3, 7]))
